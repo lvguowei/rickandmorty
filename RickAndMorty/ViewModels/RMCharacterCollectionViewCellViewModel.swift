e326927e353dc7996744bb13b29157980e86a6f7
Guowei Lv <lvguowei1002@gmail.com>
2024-02-09 14:42:16 +0200

    4:09:35

--- RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -9,21 +9,9 @@ import Foundation
 
 final class RMCharacterCollectionViewCellViewModel: Hashable, Equatable {
 
-    static func == (
-        lhs: RMCharacterCollectionViewCellViewModel, rhs: RMCharacterCollectionViewCellViewModel
-    ) -> Bool {
-        return lhs.hashValue == rhs.hashValue
-    }
-
     public let characterName: String
     private let characterStatus: RMCharacterStatus
     private let characterImageUrl: URL?
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(characterName)
-        hasher.combine(characterStatus)
-        hasher.combine(characterImageUrl)
-    }
 
     init(
         characterName: String,
@@ -45,15 +33,20 @@ final class RMCharacterCollectionViewCellViewModel: Hashable, Equatable {
             completion(.failure(URLError(URLError.badURL)))
             return
         }
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
-            guard let data = data, error == nil else {
-                completion(.failure(error ?? URLError(.badServerResponse)))
-                return
-            }
-
-            completion(.success(data))
-        }
-        task.resume()
+        RMImageLoader.shared.downloadImage(url, completion: completion)
     }
+
+    // Mark: - Hashable
+    static func == (
+        lhs: RMCharacterCollectionViewCellViewModel, rhs: RMCharacterCollectionViewCellViewModel
+    ) -> Bool {
+        return lhs.hashValue == rhs.hashValue
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(characterName)
+        hasher.combine(characterStatus)
+        hasher.combine(characterImageUrl)
+    }
+
 }
